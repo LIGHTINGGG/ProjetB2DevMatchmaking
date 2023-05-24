4b16ff3d84a90e770706321f5930767ba5870457
LIGHTINGGG contact <jules.tambon@ynov.com>
2023-05-24 12:18:09 +0200

    checkpoint à la bien

--- public/shaguer/src/components/dashboard.vue
+++ public/shaguer/src/components/dashboard.vue
@@ -1,6 +1,13 @@
 <template>
     <div id="dashboard">
         <h3>Dashboard</h3>
+
+
+        <div class="fixed-action-btn"></div>
+        <router-link to="/new" class="btn-floating 
+        btn-large red">
+
+        </router-link>
     </div>
 </template>
 
